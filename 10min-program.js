c22bf049b2ccfc104daeb05a9bf2cfdee210a489
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-07-13 00:37:55 +0300

    coding with codejika

---
+++ 10min-program.js
@@ -0,0 +1,4 @@
+<h1>Can I join the party? </h1>
+Year born: <input id="year">
+<br> <br>
+<button onclick='alert(getElementById("year").value < 2005 ? "Old enough" : "plelase grow up a little more")'> check now </button>
